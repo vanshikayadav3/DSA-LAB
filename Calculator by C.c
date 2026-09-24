ce0fce9a598875e4b16f2190e1843056e23ed7e0
vanshikayadav3 <vanshika.yadav.cse.2025@miet.ac.in>
2026-09-24 15:14:51 +0530

    Add files via upload

---
+++ Calculator by C.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,addition,subtraction,multiplication,division;
+    printf("Enter 1st no.\n");
+    scanf("%d",&a);
+    printf("Enter 2nd no.\n");
+    scanf("%d",&b);
+    addition=a+b;
+    printf("Addition is %d \n",addition);
+    subtraction=a-b;
+    printf("Subtraction is %d \n",subtraction); 
+    multiplication=a*b;
+    printf("Multiplication is %d \n",multiplication);
+    division=a/b;
+    printf("Division is %d \n",division);
+    return 0;
+
+
+
+}
